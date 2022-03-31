276789f458447285f45aad14e48f5997918437b7
Muzammil Peer <muzammilpeer987@gmail.com>
2022-03-31 15:38:47 +0400

    pytest with test db added with fixtures and loaded

---
+++ ecommerce/tests/fixtures.py
@@ -0,0 +1,18 @@
+import pytest
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+@pytest.fixture
+def create_admin_user(django_user_model):
+    """
+    create django admin user
+    """
+    return django_user_model.objects.create_superuser("root","muzammilpeer987@gmail.com","pa5is8an")
+
+@pytest.fixture(scope="session")
+def db_fixture_setup(django_db_setup,django_db_blocker):
+    """
+    setup db
+    """
+    with django_db_blocker.unblock():
+        call_command("loaddata","db_admin_fixture.json")
