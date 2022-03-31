858ed7298f396d1c1966c478fcb6c9e38398ffa9
Muzammil Peer <muzammilpeer987@gmail.com>
2022-03-31 15:50:29 +0400

    pip install black code formatter for vscode added with settings

--- ecommerce/dashboard/tests/test_ selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_ selenium_dashboard.py
@@ -1,28 +1,24 @@
 import pytest
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from django.contrib.auth.models import User
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-# @pytest.mark.selenium
-# def test_create_admin_user(create_admin_user):
-#     assert create_admin_user.__str__() == "root"
-
-# def test_dashboard_admin_login(live_server,create_admin_user,chrome_browser_instance):
 
 @pytest.mark.selenium
-def test_dashboard_admin_login(live_server,db_fixture_setup,chrome_browser_instance):
+def test_dashboard_admin_login(
+    live_server, db_fixture_setup, chrome_browser_instance
+):
     user = User.objects.get(id=1)
     print(user.password)
     browser = chrome_browser_instance
-    browser.get(("%s%s" % (live_server.url,"/admin/login/")))
+    browser.get(("%s%s" % (live_server.url, "/admin/login/")))
 
-    user_name = browser.find_element(By.NAME,"username")
+    user_name = browser.find_element(By.NAME, "username")
     user_password = browser.find_element(By.NAME, "password")
-    submit = browser.find_element(By.XPATH,'//input[@value="Log in"]')
+    submit = browser.find_element(By.XPATH, '//input[@value="Log in"]')
 
     user_name.send_keys("root")
     user_password.send_keys("pa5is8an")
     submit.send_keys(Keys.RETURN)
 
     assert "Site administration" in browser.page_source
-
--- ecommerce/tests/selenium.py
+++ ecommerce/tests/selenium.py
@@ -3,13 +3,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 @pytest.fixture(scope="module")
 def chrome_browser_instance(request):
     """
-    chrome browser web driver instance 
+    chrome browser web driver instance
     """
     options = Options()
     options.headless = False
-    browser = webdriver.Chrome("/Users/muzammilpeer/Git/python/ecommerce-app/chromedriver",chrome_options=options)
+    browser = webdriver.Chrome(
+        "/Users/muzammilpeer/chromedriver/chromedriver",
+        chrome_options=options,
+    )
     yield browser
     browser.close()
