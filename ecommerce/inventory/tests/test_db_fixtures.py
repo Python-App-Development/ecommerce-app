5d3b549511fdeb5bf7555e6693f9b70fcb4ca579
Muzammil Peer <muzammilpeer987@gmail.com>
2022-04-02 00:22:09 +0400

    second part 2 all fixtures added.
    https://www.youtube.com/watch?v=s3HuIRD5sUY&list=PLOLrQ9Pn6cay_cQkyg-WYYiJ_EKU8KWKh&index=2

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -1,6 +1,3 @@
-from unicodedata import category
-from unittest import result
-
 import pytest
 from django.db import IntegrityError
 from ecommerce.inventory import models
@@ -8,7 +5,7 @@ from ecommerce.inventory import models
 
 @pytest.mark.dbfixture
 @pytest.mark.parametrize(
-    "id, name, slug,is_active",
+    "id, name, slug, is_active",
     [
         (1, "fashion", "fashion", 1),
         (18, "trainers", "trainers", 1),
@@ -25,7 +22,7 @@ def test_inventory_category_dbfixture(
 
 
 @pytest.mark.parametrize(
-    "slug,is_active",
+    "slug, is_active",
     [
         ("fashion", 1),
         ("trainers", 1),
@@ -40,10 +37,9 @@ def test_inventory_db_category_insert_data(
     assert result.is_active == is_active
 
 
-# product
 @pytest.mark.dbfixture
 @pytest.mark.parametrize(
-    "id, web_id,name, slug,description,is_active,created_at,updated_at",
+    "id, web_id, name, slug, description, is_active, created_at, updated_at",
     [
         (
             1,
@@ -67,7 +63,7 @@ def test_inventory_db_category_insert_data(
         ),
     ],
 )
-def test_inventory_product_dbfixture(
+def test_inventory_db_product_dbfixture(
     db,
     django_db_setup,
     id,
@@ -101,11 +97,11 @@ def test_inventory_db_product_uniqueness_integrity(db, product_factory):
 def test_inventory_db_product_insert_data(
     db, product_factory, category_factory
 ):
+
     new_product = product_factory.create(category=(1, 2, 3, 4, 5))
-    result_product_category = new_product.category.all()
-    print(result_product_category)
+    result_product_category = new_product.category.all().count()
     assert "web_id_" in new_product.web_id
-    assert result_product_category.count() == 5
+    assert result_product_category == 5
 
 
 @pytest.mark.dbfixture
@@ -310,3 +306,69 @@ def test_inventory_db_stock_insert_data(db, stock_factory):
     assert new_stock.product_inventory.sku == "123456789"
     assert new_stock.units == 2
     assert new_stock.units_sold == 100
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, name, description",
+    [
+        (1, "men-shoe-size", "men shoe size"),
+    ],
+)
+def test_inventory_db_product_attribute_dataset(
+    db, db_fixture_setup, id, name, description
+):
+    result = models.ProductAttribute.objects.get(id=id)
+    assert result.name == name
+    assert result.description == description
+
+
+def test_inventory_db_product_attrubite_insert_data(
+    db, product_attribute_factory
+):
+    new_attribute = product_attribute_factory.create()
+    assert new_attribute.name == "attribute_name_0"
+    assert new_attribute.description == "description_0"
+
+
+def test_inventory_db_product_attrubite_uniqueness_integrity(
+    db, product_attribute_factory
+):
+    product_attribute_factory.create(name="not_unique")
+    with pytest.raises(IntegrityError):
+        product_attribute_factory.create(name="not_unique")
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id, product_attribute, attribute_value",
+    [
+        (1, 1, 10),
+    ],
+)
+def test_inventory_db_product_attribute_dataset(
+    db, db_fixture_setup, id, product_attribute, attribute_value
+):
+    result = models.ProductAttributeValue.objects.get(id=1)
+    assert result.product_attribute.id == 1
+    assert result.attribute_value == "10"
+
+
+def test_inventory_db_product_attribute_value_data(
+    db, product_attribute_value_factory
+):
+    new_attribute_value = product_attribute_value_factory.create(
+        attribute_value="new_value", product_attribute__name="new_value"
+    )
+    assert new_attribute_value.attribute_value == "new_value"
+    assert new_attribute_value.product_attribute.name == "new_value"
+
+
+def test_inventory_db_insert_inventory_product_values(
+    db, product_with_attribute_values_factory
+):
+
+    new_inv_attribute = product_with_attribute_values_factory(sku="123456789")
+    result = models.ProductInventory.objects.get(sku="123456789")
+    count = result.attribute_values.all().count()
+    assert count == 2
